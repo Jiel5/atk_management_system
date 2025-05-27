e73face5d1c595f8bb07b390816873230690e010
Jiel5 <jielios5@gmail.com>
2025-05-27 22:50:18 +0700

    update logo favicon dan button batal

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -4,7 +4,7 @@
             <img src="{{ asset('assets/images/logo.png') }}" alt="logo">
         </a>
         <a class="navbar-brand brand-logo-mini" href="#" style="pointer-events: none;">
-            <img src="{{ asset('assets/images/logo.jpeg') }}" alt="logo" style="height: 60px; width: auto;">
+            <img src="{{ asset('assets/images/logo.jpeg') }}" alt="logo">
         </a>
     </div>
 
